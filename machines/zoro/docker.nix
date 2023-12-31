ab8202f70b843fa2e940887a02ec310f545ccbc4
Vijayakumar Ravi <me@vijayakumar.xyz>
2023-12-31 23:14:28 +0530

    🎉 feat(zoro): added systemd service to init ariang docker volumes
    Signed-off-by: Vijayakumar Ravi <me@vijayakumar.xyz>

--- machines/zoro/docker.nix
+++ machines/zoro/docker.nix
@@ -76,4 +76,36 @@ systemd.services."docker-ariang-fix" = {
     User = "root";
   };
 };
+
+  systemd.timers."docker-ariang-volume-init" = {
+    wantedBy = [ "sysinit.target" ];
+    after = [ "docker.service" ];
+    before = [ "docker-ariang.service" ];
+    timerConfig = {
+      OnBootSec = "2m";
+      Unit = "docker-ariang-fix.service";
+    };
+  };
+
+  systemd.services."docker-ariang-volume-init" = {
+    description = "Create the docker volume for ariang.";
+    before = [ "docker-ariang.service" ];
+    serviceConfig.Type = "oneshot";
+    serviceConfig.User = "root";
+    script = ''
+      # Put a true at the end to prevent getting non-zero return code, which will crash the whole service.
+      checkariang=$( ${pkgs.docker}/bin/docker volume ls | ${pkgs.gnugrep}/bin/grep  -E '(^|\s)ariang($|\s)' || true)
+      checkariangdata=$( ${pkgs.docker}/bin/docker volume ls | ${pkgs.gnugrep}/bin/grep -E '(^|\s)ariang-data($|\s)' || true)
+      if [ -z "$checkariang" ]; then
+        ${pkgs.docker}/bin/docker volume create ariang
+      else
+          echo "ariang volume already exists in docker"
+      fi
+      if [ -z "$checkariangdata" ]; then
+        ${pkgs.docker}/bin/docker volume create ariang-data
+      else
+          echo "ariang-data volume already exists in docker"
+      fi
+    '';
+  };
 }
