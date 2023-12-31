c0c02b4617ed99edfc14117d67ca5fb4ebee466a
Vijayakumar Ravi <me@vijayakumar.xyz>
2024-01-01 00:23:35 +0530

    Fix(zoro); fixed some ariang bugs
    Signed-off-by: Vijayakumar Ravi <me@vijayakumar.xyz>

--- machines/zoro/docker.nix
+++ machines/zoro/docker.nix
@@ -1,81 +1,72 @@
 { pkgs, inputs, config, ... }: {
-  imports =
-    [
-      inputs.sops-nix.nixosModules.sops
-    ];
+  imports = [ inputs.sops-nix.nixosModules.sops ];
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
   sops.age.keyFile = "/home/vijay/.config/sops/age/keys.txt";
 
-  sops.secrets."containers/ariang/aria2_user" = {
-    owner = "root";
-  };
-  sops.secrets."containers/ariang/aria2_pass" = {
-    owner = "root";
-  };
-  sops.secrets."containers/ariang/rpc_secret" = {
-    owner = "root";
-  };
+  sops.secrets."containers/ariang/aria2_user" = { owner = "root"; };
+  sops.secrets."containers/ariang/aria2_pass" = { owner = "root"; };
+  sops.secrets."containers/ariang/rpc_secret" = { owner = "root"; };
 
-# Enable docker
-virtualisation.docker = {
-  enable = true;
-  # autoPrune.enable = true; # periodically prune Docker resources.
-  # autoPrune.dates = "weekly";
-  rootless = {
+  # Enable docker
+  virtualisation.docker = {
     enable = true;
-    setSocketVariable = true;
-  };
-};
-
-# A
-virtualisation.oci-containers = {
-  backend = "docker";
-  containers = {
-    ariang = {
-      image = "wahyd4/aria2-ui";
-      ports = [ "8080:80" "8443:443" ];
-      autoStart = true;
-      environment = {
-        PUID = "1000";
-        PGID = "1000";
-        ENABLE_AUTH = "true";
-        ARIA2_SSL = "false";
-        ARIA2_USER = builtins.readFile config.sops.secrets."containers/ariang/aria2_user".path;
-        ARIA2_PWD = builtins.readFile config.sops.secrets."containers/ariang/aria2_pass".path;
-        RPC_SECRET = builtins.readFile config.sops.secrets."containers/ariang/rpc_secret".path;
-        CADDY_LOG_LEVEL = "ERROR";
-      };
-      # workdir = "/home/vijay/docker/ariang";
-      volumes = [
-        "ariang:/app"
-        "ariang-date:/data"
-      ];
+    autoPrune.enable = true; # periodically prune Docker resources.
+    autoPrune.dates = "daily";
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
     };
   };
-};
 
-systemd.timers."docker-ariang-fix" = {
-  wantedBy = [ "sysinit.target" ];
-  after = [ "docker-ariang.service" ];
-  timerConfig = {
-    OnBootSec = "2m";
-    Unit = "docker-ariang-fix.service";
+  # A
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      ariang = {
+        image = "wahyd4/aria2-ui";
+        ports = [ "8080:80" "8443:443" ];
+        autoStart = true;
+        environment = {
+          PUID = "1000";
+          PGID = "1000";
+          ENABLE_AUTH = "true";
+          ARIA2_SSL = "false";
+          ARIA2_USER = builtins.readFile
+            config.sops.secrets."containers/ariang/aria2_user".path;
+          ARIA2_PWD = builtins.readFile
+            config.sops.secrets."containers/ariang/aria2_pass".path;
+          RPC_SECRET = builtins.readFile
+            config.sops.secrets."containers/ariang/rpc_secret".path;
+          CADDY_LOG_LEVEL = "ERROR";
+        };
+        # workdir = "/home/vijay/docker/ariang";
+        volumes = [ "ariang:/app" "ariang-data:/data" ];
+      };
+    };
   };
-};
 
-systemd.services."docker-ariang-fix" = {
-  description = "NFS permission fix for ariang container";
-  script = ''
-    set -eu
-    ${pkgs.coreutils}/bin/chmod -Rv 777 var/lib/docker/volumes/
-  '';
-  serviceConfig = {
-    Type = "oneshot";
-    User = "root";
+  systemd.timers."docker-ariang-fix" = {
+    wantedBy = [ "sysinit.target" ];
+    after = [ "docker-ariang.service" ];
+    timerConfig = {
+      OnBootSec = "2m";
+      Unit = "docker-ariang-fix.service";
+    };
   };
-};
+
+  systemd.services."docker-ariang-fix" = {
+    description = "NFS permission fix for ariang container";
+    script = ''
+      set -eu
+      ${pkgs.coreutils}/bin/chmod -Rv 777 var/lib/docker/volumes/
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
 
   systemd.timers."docker-ariang-volume-init" = {
     wantedBy = [ "sysinit.target" ];
@@ -83,7 +74,7 @@ systemd.services."docker-ariang-fix" = {
     before = [ "docker-ariang.service" ];
     timerConfig = {
       OnBootSec = "2m";
-      Unit = "docker-ariang-fix.service";
+      Unit = "docker-ariang-volume-init.service";
     };
   };
 
