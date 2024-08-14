be00b68bf00a6a5d4e65d308a79ec3593d288ed4
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-08-14 12:04:07 +0530

    Chore👨‍💻(agenix): updated usopp host key
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
 
   kakashi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
   zoro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
-  usopp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhZX0lXPldSqCLhdreywxzgXSct2XEvDsmFDrpMhk80 root@usopp";
+  usopp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEHsRr3iq20g3CmsaLYmohTX6TweobdilvDtYrzeN20 root@usopp";
 
   all = [ vijay kakashi zoro usopp ];
 in
