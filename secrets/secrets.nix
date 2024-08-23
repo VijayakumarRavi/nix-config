8e31a6df0e97eb93a04ad5b99670a111b16b7e99
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-08-23 15:50:44 +0530

    Chore👨‍💻(nami): added nami host key
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,8 +5,9 @@ let
   kakashi-nix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP6p9kIXeVoJWI2OozUxAFmjY/qbLDQ2UBh5zmrR+h3r root@kakashi-nix";
   zoro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDR0qLUZKqwEcUVgsylu53YjX3k24ZDMAhZC6R1O3jxA root@zoro";
   usopp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEHsRr3iq20g3CmsaLYmohTX6TweobdilvDtYrzeN20 root@usopp";
+  nami = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdt5PmqUqHNwEd3CoP2hXHd5uUxS9gg0R0YOU97tqSI root@nami";
 
-  all = [vijay kakashi zoro usopp kakashi-nix];
+  all = [vijay kakashi zoro usopp kakashi-nix nami];
 in {
   "userpassword".publicKeys = all;
   "kubetoken".publicKeys = all;
