47f3f23b737d183b84c3b1ccd4ae9c2652c58674
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-08-26 10:33:36 +0530

    Feat 🎉(home): store .direnv in cache instead of project dir
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- home-manager/common/common.nix
+++ home-manager/common/common.nix
@@ -1,4 +1,8 @@
-{variables, ...}: {
+{
+  config,
+  variables,
+  ...
+}: {
   programs = {
     htop.enable = true;
 
@@ -17,6 +21,16 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      stdlib = ''
+        # stolen from @i077; store .direnv in cache instead of project dir
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+                echo -n "${config.xdg.cacheHome}"/direnv/layouts/
+                echo -n "$PWD" | shasum | cut -d ' ' -f 1
+            )}"
+        }
+      '';
     };
 
     atuin = {
