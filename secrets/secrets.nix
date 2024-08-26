2d894c616c3d3ee41c26d3bf8237c14c92bf8ed5
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-08-26 20:07:28 +0530

    Chore 👨‍💻(nami): changed container backend from podman to docker and also rekeyed secrets with nami new host key
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   kakashi-nix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP6p9kIXeVoJWI2OozUxAFmjY/qbLDQ2UBh5zmrR+h3r root@kakashi-nix";
   zoro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDR0qLUZKqwEcUVgsylu53YjX3k24ZDMAhZC6R1O3jxA root@zoro";
   usopp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEHsRr3iq20g3CmsaLYmohTX6TweobdilvDtYrzeN20 root@usopp";
-  nami = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdt5PmqUqHNwEd3CoP2hXHd5uUxS9gg0R0YOU97tqSI root@nami";
+  nami = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGy2OwcZHvlrOisEOQ22j0rx8bhKPUSGKNTmmH3u+cvu root@nami";
 
   all = [vijay kakashi zoro usopp kakashi-nix nami];
 in {
