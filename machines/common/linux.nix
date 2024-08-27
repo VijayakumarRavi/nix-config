4cdd26466d849f14b711debdff7c2c4f6761eab4
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-08-28 00:54:35 +0530

    Chore👨‍💻(flake): Disabled watchdog on nami
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- machines/common/linux.nix
+++ machines/common/linux.nix
@@ -122,8 +122,6 @@
     libinput.enable = true;
   };
 
-  systemd.services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
-
   age.secrets = {
     kubetoken = {file = ../../secrets/kubetoken;};
     tailauthKeyFile = {file = ../../secrets/tailauthKeyFile;};
@@ -151,4 +149,35 @@
       fi
     '';
   };
+  systemd =
+    if meta.hostname != "nami"
+    then {
+      # Given that systems are headless, emergency mode is useless.
+      # We prefer the system to attempt to continue booting so
+      # that we can hopefully still access it remotely.
+      enableEmergencyMode = false;
+
+      # For more detail, see:
+      #   https://0pointer.de/blog/projects/watchdog.html
+      watchdog = {
+        # systemd will send a signal to the hardware watchdog at half
+        # the interval defined here, so every 10s.
+        # If the hardware watchdog does not get a signal for 20s,
+        # it will forcefully reboot the system.
+        runtimeTime = "20s";
+        # Forcefully reboot if the final stage of the reboot
+        # hangs without progress for more than 30s.
+        # For more info, see:
+        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+        rebootTime = "30s";
+      };
+
+      sleep.extraConfig = ''
+        AllowSuspend=no
+        AllowHibernation=no
+      '';
+
+      services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
+    }
+    else {services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];};
 }
