34b5e02c814c954c4ef73cdcf256be3f3a2fcf86
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-09-16 18:03:24 +0530

    Chore👨‍💻(dotfiles): added some git config & move htop config to nix
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

---
+++ home-manager/core/default.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  config,
+  variables,
+  ...
+}: {
+  imports = [
+    ./yazi
+    ./core.nix
+    ./htop.nix
+    ./zsh.nix
+    ./git.nix
+    ./tmux.nix
+    ./starship.nix
+  ];
+
+  systemd.user.startServices = "sd-switch";
+  home = {
+    sessionPath = ["$HOME/.local/bin"];
+    sessionVariables = {
+      PAGER = "less";
+      CLICLOLOR = 1;
+      EDITOR = "nvim";
+    };
+    # Don't change this when you change package input. Leave it alone.
+    inherit (variables) stateVersion;
+  };
+  home.activation.report-changes = config.lib.dag.entryAnywhere ''
+    echo "--- home manager diff to current-system"
+    ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff $oldGenPath $newGenPath
+    echo "---"
+  '';
+}
