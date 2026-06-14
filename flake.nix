b3616aadc6e8502c9f0fe6bff5578409e4735a84
Vijayakumar Ravi <im@vijayakumar.xyz>
2026-06-14 17:16:35 +0530

    :hammer_and_wrench: chore(cache): added atticfly cache
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org"
-      "https://vijay.cachix.org"
+      "https://atticfly.fly.dev/system?priority=43"
       "https://numtide.cachix.org"
       "https://nix-community.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      "vijay.cachix.org-1:6Re6EF3Q58sxaIobAWP1QTwMUCSA0nYMrSJGUedL3Zk="
+      "system:4DUyL9UsLftxdxGfmAyxfU5TnQc+8vD/uRBryYSonss="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
